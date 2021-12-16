864fb59c7277a19861af2f9471475b6f106aa4ba
siranjiv <siranjiv@gmail.com>
2021-12-16 08:03:18 +0530

    The Java doc and azure

---
+++ TheRiddlesGame/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"DatabaseLayer"},{"l":"Engine"},{"l":"GUI"},{"l":"Model"},{"l":"rmiInterface"},{"l":"Server"}];updateSearchResults();
